b4fdf6f9507c1affe283021b40567b369ee10888
luizjedi <luiz.pepe@hotmail.com>
2021-04-28 00:52:56 -0300

    Predicate - Implementação da interface

--- Secao20_ProgramacaoFuncional/src/entities/Product.java
+++ Secao20_ProgramacaoFuncional/src/entities/Product.java
@@ -29,7 +29,7 @@ public class Product {
 
 	@Override
 	public String toString() {
-		return "Product [name=" + name + ", price=" + price + "]";
+		return name + ", " + price;
 	}
 
 }
---
+++ Secao20_ProgramacaoFuncional/src/util/ProductPredicate.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class ProductPredicate implements Predicate<Product> {
+
+	@Override
+	public boolean test(Product t) {
+		
+		return t.getPrice() >= 100.0;
+	}
+	
+
+}
