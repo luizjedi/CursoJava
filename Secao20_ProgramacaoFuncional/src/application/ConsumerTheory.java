e66faac69ab4bf534baf4b60bafad46c4ff3fc71
luizjedi <luiz.pepe@hotmail.com>
2021-04-28 01:28:21 -0300

    Consumer - Expressão lambda inline

--- Secao20_ProgramacaoFuncional/src/application/ConsumerTheory.java
+++ Secao20_ProgramacaoFuncional/src/application/ConsumerTheory.java
@@ -3,14 +3,12 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
-
 import entities.Product;
 
 public class ConsumerTheory {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 
@@ -18,12 +16,9 @@ public class ConsumerTheory {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
-		
-		Consumer<Product> pred = p -> p.setPrice(p.getPrice() * 1.1);
-		
-		list.forEach(pred);
+
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 		list.forEach(System.out::println);
-		
 
 	}
 
