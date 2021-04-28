a0e4a00b59e9a57d3b8d2817ee8e2df3139efed4
luizjedi <luiz.pepe@hotmail.com>
2021-04-28 01:08:54 -0300

    Predicate - Expressão lambda inline

--- Secao20_ProgramacaoFuncional/src/application/PredicateTheory.java
+++ Secao20_ProgramacaoFuncional/src/application/PredicateTheory.java
@@ -2,25 +2,23 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-
 import entities.Product;
 
 public class PredicateTheory {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
-		
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
-		
-		list.removeIf(pred);
-		
+
+		double min = 100.0;
+
+		list.removeIf(p -> p.getPrice() >= min);
+
 		for (Product p : list) {
 			System.out.println(p);
 		}
