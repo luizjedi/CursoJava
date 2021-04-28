387927f585ef48acf6f05ba1122751f62ba3883e
luizjedi <luiz.pepe@hotmail.com>
2021-04-28 01:27:31 -0300

    Consumer - Expressão lambda declarada

--- Secao20_ProgramacaoFuncional/src/application/ConsumerTheory.java
+++ Secao20_ProgramacaoFuncional/src/application/ConsumerTheory.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,7 +19,9 @@ public class ConsumerTheory {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::noStaticPriceUpdate);
+		Consumer<Product> pred = p -> p.setPrice(p.getPrice() * 1.1);
+		
+		list.forEach(pred);
 		list.forEach(System.out::println);
 		
 
